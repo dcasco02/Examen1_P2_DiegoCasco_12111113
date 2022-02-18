6ec7470537fdafe5ac9f11075a822dc60828483c
dcasco02 <dcasco@unitec.edu>
2022-02-18 16:42:24 -0600

    Avance 5

--- src/examen1_p2_diegocasco_12111113/Examen1_P2_DiegoCasco_12111113.java
+++ src/examen1_p2_diegocasco_12111113/Examen1_P2_DiegoCasco_12111113.java
@@ -75,7 +75,8 @@ public class Examen1_P2_DiegoCasco_12111113 {
                             + "1.1-Crear Escuadron\n"
                             + "1.2-Modificar universo\n"
                             + "1.3-Eliminar universos\n"
-                            + "1.4-Listar"));
+                            + "1.4-Listar\n"
+                            + "1.5-Agregar a persona"));
                     if(opcion==1){
                         String name=JOptionPane.showInputDialog("Ingrese el nombre del personaje: ");
                         String lugar=JOptionPane.showInputDialog("Ingrese el lugar(puebo o ciudad): ");
@@ -85,9 +86,99 @@ public class Examen1_P2_DiegoCasco_12111113 {
                         }else if(goodobad=="villan"){
                             heroe=false;
                         }
-                        universos.add(new Escuadron(name, lugar, goodobad, universos,true, lider));
+                        universos.add(new Escuadron(name, lugar, goodobad, universos, true, lider));
                     }
-                }    
+                }
+            }
+            if (opcion == 3) {
+                int opcion2 = 7;
+                while (opcion2 != 0) {
+                    opcion2 = Integer.parseInt(
+                            JOptionPane.showInputDialog("0-salir"
+                                    + "3.1-Crear persona\n"
+                                    + "3.2-Modificar persona\n"
+                                    + "3.3-Eliminar persona\n"
+                                    + "3.4-Listar"));
+                    if (opcion == 1) {
+                        int opcion3 = 9;
+                        while (opcion3 != 0) {
+                            opcion3 = Integer.parseInt(
+                                    JOptionPane.showInputDialog("1-normal\n"
+                                            + "2-alien\n"
+                                            + "3-Extraterrestre\n"
+                                            + "3-diety\n"
+                                            + "4-mutante\n"
+                                            + "5-radioactivo\n"
+                                            + "6-superhumano\n"
+                                            + "0-salir"));
+                            if (opcion == 1) {
+                                String name = JOptionPane.showInputDialog("Ingrese el nombre del personaje: ");
+                                String debilidad = JOptionPane.showInputDialog("Ingrese la debilidad del personaje: ");
+                                String poder = JOptionPane.showInputDialog("Ingrese el poder del personaje: ");
+                                String status = JOptionPane.showInputDialog("Ingrese el heroeovillano del personaje: ");
+                                int fuerza = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la fuerza de la persona "));
+                                int mental = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la habilidad mental de la persona: "));
+                                int fisica = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la habilidad fisica de la persona: "));
+                                personas.add(new normal(name, debilidad, poder, debilidad, fuerza, mental, fisica, true));
+                            }
+                            if (opcion == 2) {
+                                String name = JOptionPane.showInputDialog("Ingrese el nombre del personaje: ");
+                                String debilidad = JOptionPane.showInputDialog("Ingrese la debilidad del personaje: ");
+                                String poder = JOptionPane.showInputDialog("Ingrese el poder del personaje: ");
+                                String status = JOptionPane.showInputDialog("Ingrese el heroeovillano del personaje: ");
+                                String planeta = JOptionPane.showInputDialog("Ingrese el planeta del personaje: ");
+                                int fuerza = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la fuerza de la persona "));
+                                int mental = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la habilidad mental de la persona: "));
+                                int fisica = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la habilidad fisica de la persona: "));
+                                personas.add(new alien(poder, name, debilidad, poder, debilidad, fuerza, mental, fisica, true));
+                            }
+                            if (opcion == 3) {
+                                String name = JOptionPane.showInputDialog("Ingrese el nombre del personaje: ");
+                                String debilidad = JOptionPane.showInputDialog("Ingrese la debilidad del personaje: ");
+                                String poder = JOptionPane.showInputDialog("Ingrese el poder del personaje: ");
+                                String status = JOptionPane.showInputDialog("Ingrese el heroeovillano del personaje: ");
+                                int fuerza = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la fuerza de la persona "));
+                                int mental = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la habilidad mental de la persona: "));
+                                int fisica = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la habilidad fisica de la persona: "));
+                                personas.add(new extraterrestre(name, debilidad, poder, debilidad, fuerza, mental, fisica, true));
+                            }
+                            if (opcion == 4) {
+                                String name = JOptionPane.showInputDialog("Ingrese el nombre del personaje: ");
+                                String debilidad = JOptionPane.showInputDialog("Ingrese la debilidad del personaje: ");
+                                String poder = JOptionPane.showInputDialog("Ingrese el poder del personaje: ");
+                                String status = JOptionPane.showInputDialog("Ingrese el heroeovillano del personaje: ");
+                                String religion = JOptionPane.showInputDialog("Ingrese la religion o mitologia del personaje: ");
+                                int fuerza = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la fuerza de la persona "));
+                                int mental = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la habilidad mental de la persona: "));
+                                int fisica = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la habilidad fisica de la persona: "));
+                                personas.add(new diety(true, religion, name, debilidad, poder, religion, fuerza, mental, fisica, true));
+                            }
+                            if (opcion == 5) {
+                                String name = JOptionPane.showInputDialog("Ingrese el nombre del personaje: ");
+                                String debilidad = JOptionPane.showInputDialog("Ingrese la debilidad del personaje: ");
+                                String poder = JOptionPane.showInputDialog("Ingrese el poder del personaje: ");
+                                String status = JOptionPane.showInputDialog("Ingrese el heroeovillano del personaje: ");
+                                String accidente = JOptionPane.showInputDialog("Ingrese el tippo de accidente del personaje: ");
+                                int fuerza = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la fuerza de la persona "));
+                                int mental = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la habilidad mental de la persona: "));
+                                int fisica = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la habilidad fisica de la persona: "));
+                                int edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese edad al momento de accidente de la persona: "));
+                                personas.add(new superhumano(poder, name, debilidad, poder, debilidad, fuerza, mental, fisica, true));
+                            }
+                            if (opcion == 6) {
+                                String name = JOptionPane.showInputDialog("Ingrese el nombre del personaje: ");
+                                String debilidad = JOptionPane.showInputDialog("Ingrese la debilidad del personaje: ");
+                                String poder = JOptionPane.showInputDialog("Ingrese el poder del personaje: ");
+                                String status = JOptionPane.showInputDialog("Ingrese el heroeovillano del personaje: ");
+                                String superpoder = JOptionPane.showInputDialog("Ingrese el superpoder del personaje: ");
+                                int fuerza = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la fuerza de la persona "));
+                                int mental = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la habilidad mental de la persona: "));
+                                int fisica = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la habilidad fisica de la persona: "));
+                                personas.add(new superhumano(superpoder, name, debilidad, poder, debilidad, fuerza, mental, fisica, true));
+                            }
+                        }
+                    }
+                }
             }
         }
     }
